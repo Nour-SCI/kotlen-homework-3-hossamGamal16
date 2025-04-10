a4e2ed11f7934100441950c6c10467c3bbe41f72
hossam <hossamg16@gmail.com>
2025-04-10 18:49:42 +0200

    6

--- module1/src/main/kotlin/QuickSort.kt
+++ module1/src/main/kotlin/QuickSort.kt
@@ -1,18 +1,13 @@
 import kotlin.random.Random
 import utils.getStringRepresentation
-
 fun <T : Comparable<T>> quickSort(items: List<T>): List<T> {
     if (items.size < 2) return items
-
     val pivot = items[items.size / 2]
     val less = items.filter { it < pivot }
     val equal = items.filter { it == pivot }
     val greater = items.filter { it > pivot }
-
     return quickSort(less) + equal + quickSort(greater)
 }
-
-
 fun main() {
     println("Before quicksort:")
     val numbers = List(10) { Random.nextInt(0, 100) }
